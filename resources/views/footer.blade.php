ca71fdbd680df247f333a7788714c01ea973d1c5
Alex <2016030891@upsin.edu.mx>
2019-03-15 01:33:09 -0700

    Cambios: 1.Diseño,2.y-scrolling,3.Actualización de angularjs.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -12,7 +12,7 @@
         <form>
           <div class="form-group">
             <label for="exampleFormControlInput1">Nombre</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="nombre">
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="nombre" only-letters-input>
           </div>
           <div class="form-group">
             <label for="exampleFormControlInput1">Correo electrónico</label>
@@ -31,7 +31,6 @@
               <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="número de ticket">
             </div>
           </div>
-
           <button type="submit" class="btn btn-light">Enviar</button>
         </form>
       </div>
@@ -58,10 +57,6 @@
   <!-- Copyright -->
 </footer>
 <!-- Footer -->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </div>
   <script type="text/javascript" src="{{asset('/js/angular.js')}}"></script>
 </body>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,9 @@
 @section('header')
 @parent
     <div class="" ng-controller="ctrl">
-      <nav class="navbar navbar-expand-lg fixed-top" id="navbar"><!-- bg-light, navbar-light-->
-          <img src="{{asset('images/DESOFAM_LOGO.png')}}" height="40" alt="Logo de DESOFAM">
-          <a>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <nav class="navbar navbar-expand-lg fixed-top d-flex justify-content-center" id="navbar">
+          <a class="navbar-brand" href="#"><img src="{{asset('images/DESOFAM_LOGO.png')}}" height="40" alt="Logo de DESOFAM"></a>
+          <div class="collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
                 <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
@@ -25,7 +24,6 @@
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <a class="dropdown-item" href="#nosotros">¿Quiénes somos?</a>
                   <a class="dropdown-item" href="#mision">Misión</a>
-                  <!--<div class="dropdown-divider">Visión</div>-->
                   <a class="dropdown-item" href="#vision">Visión</a>
                   <a class="dropdown-item" href="#pFrecuentes">Preguntas frecuentes</a>
                 </div>
@@ -34,7 +32,7 @@
           </div>
         </nav>
 
-        <div class="bd-example"><!-- w-100  -->
+        <div class="bd-example w-100">
           <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
               <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
@@ -49,39 +47,20 @@
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="{{asset('images/1.jpg')}}" class="d-block w-100 imgCarrusel" alt="Segundo proyecto chido" id=""><!--w-100-->
+                <img src="{{asset('images/1.jpg')}}" class="d-block w-100 imgCarrusel" alt="Segundo proyecto chido" id="">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>Segundo noticia</h5>
+                  <h5>Segunda noticia</h5>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
               </div>
             </div>
-            <!--<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>-->
-            </div>
           </div>
-          </div>
-
-      <!--<div class="bgimg-1">
-        <div class="caption">
-          <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Bienvenide</span>
         </div>
-      </div>-->
-
-  <!--<div class="texto" id="nosotros">
-    <h3>¿Quiénes somos?</h3>
-    <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-  </div>-->
+    <!--</div>-->
 
   <div class="bgimg-2" id="nosotros">
     <div class="caption">
-      <span class="border">¿Quiénes somos?</span><!--style="background-color:white;font-size:25px;color: black;"-->
+      <span class="border">¿Quiénes somos?</span>
     </div>
   </div>
 
@@ -91,73 +70,77 @@
     </div>
   </div>
 
-    <div class="bgimg-2 ">
-      <div class="row">
-        <div class="col-sm">
-          <div class="bgimg-2" id="mision">
-            <div class="caption">
-              <span class="border">Misión</span><!--style="background-color:white;font-size:25px;color: black;"-->
+    <div class="bgimg-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="bgimg-2" id="mision">
+              <div class="caption">
+                <span class="border">Misión</span>
+              </div>
+            </div>
+            <div class="bgimg-2 d-flex justify-content-center">
+              <div class="texto">
+                <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+              </div>
             </div>
           </div>
-          <div class="bgimg-2 d-flex justify-content-center">
-            <div class="texto">
-              <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+          <div class="col-sm">
+            <div class="bgimg-2" id="vision">
+              <div class="caption">
+                <span class="border">Visión</span>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="col-sm">
-          <div class="bgimg-2" id="vision">
-            <div class="caption">
-              <span class="border">Visión</span>
-            </div>
-          </div>
-          <div class="bgimg-2 d-flex justify-content-center">
-            <div class="texto">
-              <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+            <div class="bgimg-2 d-flex justify-content-center">
+              <div class="texto">
+                <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <div class="bgimg-2" id="portfolio">
+      <div class="caption">
+        <span class="border">Portfolio</span>
+      </div>
+    </div>
+
+    <div class="bgimg-2 d-flex justify-content-center" id="portfolio">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="{{asset('images/sistema_geriatrico_logo.png')}}" alt="Card image cap">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
 
   <div class="bgimg-2" id="pFrecuentes">
     <div class="caption">
-      <span class="border">Preguntas frecuentes</span>
+      <span class="border">
+        <a class="" data-toggle="collapse" href="#collapseFrecuentes" role="button" aria-expanded="false" aria-controls="collapseExample">
+          Preguntas frecuentes
+        </a>
+        </span>
     </div>
   </div>
 
-  <div class="texto w-100">
-    <p>1. Nascetur per nec posuere turpis
-      <br>
-      2. lectus nec libero turpis nunc at
-      <br>
-      3.sed posuere mollis ullamcorper libero ante lectus
-      <br>4. blandit pellentesque a, magna turpis est sapien duis blandit dignissim.
-      <br>5. Viverra interdum mi magna mi, morbi sociis.
-      <br>6. Condimentum dui ipsum consequat morbi, curabitur aliquam pede
-      <br>7. nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor,
-      <br>8. nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-  </div>
-
-  <div class="bgimg-2" id="portfolio">
-    <div class="caption">
-      <span class="border">Portfolio</span>
+  <div class="collapse" id="collapseFrecuentes">
+    <div class="texto w-100">
+      <p>1. Nascetur per nec posuere turpis
+        <br>
+        2. lectus nec libero turpis nunc at
+        <br>
+        3.sed posuere mollis ullamcorper libero ante lectus
+        <br>4. blandit pellentesque a, magna turpis est sapien duis blandit dignissim.
+        <br>5. Viverra interdum mi magna mi, morbi sociis.
+        <br>6. Condimentum dui ipsum consequat morbi, curabitur aliquam pede
+        <br>7. nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor,
+        <br>8. nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
     </div>
   </div>
-
-  <div class="bgimg-2 d-flex justify-content-center" id="portfolio">
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="{{asset('images/sistema_geriatrico_logo.png')}}" alt="Card image cap">
-      <div class="card-body">
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-    </div>
-  </div>
-
-    <div class="bgimg-2" id="portfolio" style="min-height:110px;"></div>
-    <!--<a href="#">Más información</a>-->
-
-    <!--<div class="bgimg-1"></div>-->
 
     <!-- Footer -->
   @section('footer')
