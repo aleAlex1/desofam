ee3d1afcff3145d2cb4eaf9f6629999b753efaf3
Alex <2016030891@upsin.edu.mx>
2019-03-24 23:02:35 -0700

    Actualización de diseño.

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,9 +5,10 @@
     <link rel="icon" href="/images/favicon.png" sizes="16x16">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Home</title>
+    <title>Desofam</title>
     <link rel="stylesheet" href="/css/app.css">
     <link rel="stylesheet" href="/css/estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body>
+  <body data-spy="scroll" data-target=".navbar" data-offset="0">
 @show
