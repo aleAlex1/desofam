325cc3c91a82ac8068d15b72acbce3de96905d4c
XaviOficial <xavialmeidaoficial@gmail.com>
2019-04-03 08:15:18 -0700

    Deshabilitar los campos de la información del usuario

--- resources/views/responderCorreosST.blade.php
+++ resources/views/responderCorreosST.blade.php
@@ -9,20 +9,20 @@
 <form name="frmUser" class="adminAdd col-lg-8" ng-controller="ctrl">
   <div class="form-group col-lg-3">
     <label for="name">Ticket</label>
-    <input type="text" class="form-control" id="name" placeholder="Ticket" ng-model="">
+    <input type="text" class="form-control" id="name" placeholder="Ticket" ng-model="" disabled>
   </div>
   <br>
   <div class="form-group col-lg-5">
     <label for="name">Nombre</label>
-    <input type="text" class="form-control" id="name" placeholder="Nombre" ng-model="">
+    <input type="text" class="form-control" id="name" placeholder="Nombre" ng-model="" disabled>
   </div>
   <div class="form-group col-lg-5">
     <label for="mail">Correo</label>
-    <input type="email" class="form-control" id="mail" placeholder="Correo" ng-model="">
+    <input type="email" class="form-control" id="mail" placeholder="Correo" ng-model="" disabled>
   </div>
   <div class="form-group col-lg-10">
     <label for="exampleFormControlTextarea1">Mensaje</label>
-    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model=""></textarea>
+    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model="" disabled></textarea>
   </div>
   <div class="form-group col-lg-10">
     <label for="exampleFormControlTextarea1">Respuesta</label>
