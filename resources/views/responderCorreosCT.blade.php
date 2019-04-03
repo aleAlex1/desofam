f73701335cf01314ef62283e5b03bb42ac05441e
XaviOficial <xavialmeidaoficial@gmail.com>
2019-04-03 07:55:18 -0700

    Deshabilitar la informacion del mensaje

--- resources/views/responderCorreosCT.blade.php
+++ resources/views/responderCorreosCT.blade.php
@@ -9,29 +9,29 @@
 <form name="frmUser" class="adminAdd col-lg-8" ng-controller="ctrl" ng-init="mostrarTicketPrevio({{$id}})"> <!--ng-init="mostrarCorreoCTicket()"-->
   <div class="form-group col-lg-3">
     <label for="name">Ticket Previo</label>
-    <input type="text" class="form-control" id="name" placeholder="Ticket Previo" ng-model="ticketPrevio.id">
+    <input type="text" class="form-control" id="name" placeholder="Ticket Previo" ng-model="ticketPrevio.id" disabled>
   </div>
   <br>
   <div class="form-group col-lg-5">
     <label for="name">Nombre</label>
-    <input type="text" class="form-control" id="name" placeholder="Nombre" ng-model="ticketPrevio.nombre">
+    <input type="text" class="form-control" id="name" placeholder="Nombre" ng-model="ticketPrevio.nombre" disabled>
   </div>
   <div class="form-group col-lg-5">
     <label for="mail">Correo</label>
-    <input type="email" class="form-control" id="mail" placeholder="Correo" ng-model="ticketPrevio.correo">
+    <input type="email" class="form-control" id="mail" placeholder="Correo" ng-model="ticketPrevio.correo" disabled>
   </div>
   <div class="form-group col-lg-10">
     <label for="exampleFormControlTextarea1">Mensaje Previo</label>
-    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model="ticketPrevio.mensajePrevio"></textarea>
+    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model="ticketPrevio.mensajePrevio" disabled></textarea>
   </div>
 
   <div class="form-group col-lg-3">
     <label for="name">Ticket Reciente</label>
-    <input type="text" class="form-control" id="name" placeholder="Ticket Reciente" ng-model="ticketActual.id">
+    <input type="text" class="form-control" id="name" placeholder="Ticket Reciente" ng-model="ticketActual.id" disabled>
   </div>
   <div class="form-group col-lg-10">
     <label for="exampleFormControlTextarea1">Mensaje Reciente</label>
-    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model="ticketActual.mensajeReciente"></textarea>
+    <textarea class="form-control" name="" id="exampleFormControlTextarea1" rows="5" ng-model="ticketActual.mensajeReciente" disabled></textarea>
   </div>
   <div class="form-group col-lg-10">
     <label for="exampleFormControlTextarea1">Respuesta</label>
